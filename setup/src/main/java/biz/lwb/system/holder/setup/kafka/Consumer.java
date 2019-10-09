49f9a964767cde8cd188c11e2ce7f7ce2a2d7d72
Tomasz Kuśmierczyk <tomasz.kusmierczyk@letsweb.biz>
2019-10-09 15:49:10 +0200

    add example Consumer

---
+++ setup/src/main/java/biz/lwb/system/holder/setup/kafka/Consumer.java
@@ -0,0 +1,18 @@
+package biz.lwb.system.holder.setup.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+@Slf4j
+public class Consumer {
+
+    @KafkaListener(topics = Producer.TOPIC, groupId = "group_id")
+    public void consume(String message) throws IOException {
+        log.info(String.format("#### -> Consumed message -> %s", message));
+    }
+
+}
--- setup/src/main/java/biz/lwb/system/holder/setup/kafka/Producer.java
+++ setup/src/main/java/biz/lwb/system/holder/setup/kafka/Producer.java
@@ -3,18 +3,31 @@ package biz.lwb.system.holder.setup.kafka;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Slf4j
 @Service
 public class Producer {
-    private static final String TOPIC = "test2";
+    public static final String TOPIC = "test2";
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    public void sendMessage(String message){
-        log.info("producing message: {}", message);
-        kafkaTemplate.send(TOPIC, message);
+    public void sendMessage(String message) {
+        ListenableFuture<SendResult<String, String>> send = kafkaTemplate.send(TOPIC, message);
+        send.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                log.error("sending log failed: ", throwable);
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, String> stringStringSendResult) {
+                log.info("producing message: {}", message);
+            }
+        });
     }
 }
